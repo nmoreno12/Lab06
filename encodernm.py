529826d6c69157bd40bcfb6718a430784443f7c5
garrettmullins <127443479+garrettmullins@users.noreply.github.com>
2023-03-22 22:59:52 -0400

    Added decoder

--- encodernm.py
+++ encodernm.py
@@ -16,10 +16,20 @@ def encode():
         encode_digit = str((int(digit) + 3) % 10)
         encoded_user_pass += encode_digit
     print('Your password has been encoded and stored!\n')
+    print(f'The encoded password is {encoded_user_pass}, and the original password is {user_pass}.\n')
     return encoded_user_pass
 
 
 #  create decoder function
+def decode(encoded_user_pass):
+
+    decoded_password = ""
+    for i in range(len(encoded_user_pass)):
+        decoded_char = ((int(encoded_user_pass[i])-3))
+        if decoded_char < 0:
+            decoded_char = 10 - decoded_char
+        decoded_password += str(decoded_char)
+    return decoded_password
 
 
 #  create main function with while loop
@@ -29,9 +39,9 @@ def main():
         menu()
         menu_selection = int(input('Please enter an option: '))
         if menu_selection == 1:
-            encode()
+            new_pass = encode()
         if menu_selection == 2:
-            pass
+            new_pass = decode(new_pass)
         if menu_selection == 3:
             run = False
 
